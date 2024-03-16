ff463892bb28c2f6938b77521d92fdf75eca85f9
Ayush Kumar <kayush2k02@gmail.com>
2024-03-16 15:31:55 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -150,5 +150,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kayush2k02@gmail.com'
-EMAIL_HOST_PASSWORD = '!heyyushcsePy3.9'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxxxxxxx'
 EMAIL_USE_TLS = True
